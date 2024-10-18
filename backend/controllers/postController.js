48ca8b2b58f56228992a0d0a9c1518b392a093a1
Andyvux <anhvuktnh@gmail.com>
2024-10-18 16:33:44 +0700

    Update full Post controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,6 +1,7 @@
 import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 
+//create Post
 const createPost = async (req, res) => {
   try {
     const { postedBy, text, img } = req.body;
@@ -29,4 +30,127 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+//get Post
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(200).json({ post });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+//Delete Post
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to delete post" });
+    }
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post delete successfully!" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//like Post
+const likeUnlikePost = async (req, res) => {
+  try {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      // Unlike post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully!" });
+    } else {
+      // Like post
+      post.likes.push(userId);
+      await post.save();
+      res.status(200).json({ message: "Post liked successfully!" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//reply Post
+const replyToPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text field is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found!" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json(reply);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+//Get Feed Posts
+const getFeedPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+
+    res.status(200).json(feedPosts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Get User Posts
+const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({ createdAt: -1 });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+export { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPosts, getUserPosts };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost, deletePost, getFeedPosts, getPost, getUserPosts, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/user", protectRoute, getUserPosts);
+router.get("/feed", protectRoute, getFeedPosts);
+router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 
 export default router;
