db871442b5d94f7f160b33b3f24b6b84a34d8a98
Andyvux <anhvuktnh@gmail.com>
2025-01-14 23:51:05 +0700

    commit

--- backend/sever.js
+++ backend/sever.js
@@ -40,11 +40,11 @@ app.use("/api/messages", messageRoutes);
 // http://localhost:5000 => backend,frontend
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // react app
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
   });
 }
 
